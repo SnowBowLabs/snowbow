2f872fa43e8fcdb140b3a417efb81ee1c34577b8
Noy <59097122+noyyyy@users.noreply.github.com>
2023-11-27 01:34:56 +0800

    feat: observer return end price if apply

--- packages/contracts/src/interfaces/IPriceObserver.sol
+++ packages/contracts/src/interfaces/IPriceObserver.sol
@@ -13,6 +13,9 @@ interface IPriceObserver {
     }
 
     // get the product status
-    // return product status and valid period(if apply)
-    function getProductResult(address productAddr) external view returns (SnowbowResultStatus, uint256 period);
+    // return product status, valid period, end price(if apply)
+    function getProductResult(address productAddr)
+        external
+        view
+        returns (SnowbowResultStatus, uint256 period, uint256 endPrice);
 }
