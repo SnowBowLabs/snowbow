888eb3ea8a58b5b70ce9daa9819b986d3ecfbab6
Noy <59097122+noyyyy@users.noreply.github.com>
2023-11-27 01:42:55 +0800

    feat: impl claim logic

--- packages/contracts/src/SnowbowSeller.sol
+++ packages/contracts/src/SnowbowSeller.sol
@@ -7,6 +7,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ISnowbowSeller} from "src/interfaces/ISnowbowSeller.sol";
 import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
+import {IPriceObserver} from "src/interfaces/IPriceObserver.sol";
 
 contract SnowbowSeller is ISnowbowSeller {
     using SafeERC20 for IERC20;
@@ -25,6 +26,8 @@ contract SnowbowSeller is ISnowbowSeller {
     uint8 _targetDecimal;
     // target token price, in per usd
     uint256 internal _targetInitPrice;
+    uint256 internal _targetKnockInPrice;
+    uint256 internal _targetKnockOutPrice;
 
     address internal _usdToken;
     mapping(address => uint256) internal _boughtAmount;
@@ -38,25 +41,34 @@ contract SnowbowSeller is ISnowbowSeller {
     // wether the user had claimed the reward
     BitMaps.BitMap internal _claimed;
 
+    address internal _priceObserver;
+
     /**
      * @dev one contract for one target asset, only support WETH/WBTC
      */
     constructor(
         address targetToken,
         uint256 targetInitPrice,
+        uint256 targetKnockInPrice,
+        uint256 targetKnockOutPrice,
         uint256 startTime,
         uint256 period,
         uint256 baseProfit,
-        address usdToken
+        address usdToken,
+        address priceObserver
     ) {
         _targetToken = targetToken;
         _targetInitPrice = targetInitPrice;
+        _targetKnockInPrice = targetKnockInPrice;
+        _targetKnockOutPrice = targetKnockOutPrice;
         _startTime = uint32(startTime);
         _period = uint32(period);
         _baseProfit = uint16(baseProfit);
         _usdToken = usdToken;
 
         _targetDecimal = IERC20Metadata(targetToken).decimals();
+
+        _priceObserver = priceObserver;
     }
 
     /**
@@ -98,12 +110,43 @@ contract SnowbowSeller is ISnowbowSeller {
 
         _claimed.set(uint160(msg.sender));
 
-        // judge the condition and give corspoding reward
-        // if not knock in nor knock out
-        uint256 rewardUSDAmount =
+        (IPriceObserver.SnowbowResultStatus status, uint256 validPeriod, uint256 endPrice) =
+            IPriceObserver(_priceObserver).getProductResult(address(this));
+
+        uint256 allPeriodRewardUSDAmount =
             _boughtAmount[msg.sender] * _targetInitPrice * (10000 + _baseProfit) / PROFIT_BASE * 10 ** _targetDecimal;
 
-        IERC20(_usdToken).safeTransfer(msg.sender, rewardUSDAmount);
+        // judge the condition and give corspoding reward
+        if (status == IPriceObserver.SnowbowResultStatus.NorInOrOut) {
+            // if not knock in nor knock out
+            // reward all reward
+            IERC20(_usdToken).safeTransfer(msg.sender, allPeriodRewardUSDAmount);
+        } else if (
+            status == IPriceObserver.SnowbowResultStatus.InAndOut
+                || status == IPriceObserver.SnowbowResultStatus.OnlyOut
+        ) {
+            // if knock in and knock out, or only knock out
+            // reward the valid period part of reward
+            IERC20(_usdToken).safeTransfer(msg.sender, allPeriodRewardUSDAmount * validPeriod / _period);
+        } else if (status == IPriceObserver.SnowbowResultStatus.OnlyIn) {
+            // if knock in but no knock out
+            if (endPrice > _targetInitPrice) {
+                // if end price larger than init price, user get a part earning
+                IERC20(_usdToken).safeTransfer(
+                    msg.sender,
+                    allPeriodRewardUSDAmount * (_targetKnockOutPrice - endPrice) / _period
+                        * (_targetKnockOutPrice - _targetKnockInPrice)
+                );
+            } else {
+                // if end price smaller than init price, user get part loss
+                IERC20(_usdToken).safeTransfer(
+                    msg.sender,
+                    allPeriodRewardUSDAmount
+                        - allPeriodRewardUSDAmount * (endPrice - _targetKnockInPrice) / _period
+                            * (_targetKnockOutPrice - _targetKnockInPrice)
+                );
+            }
+        }
     }
 
     /**
