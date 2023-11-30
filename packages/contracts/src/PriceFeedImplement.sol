3b79ccb3cf673dd31f441e731f80756d76d820ab
c <anyone.cui935@gmail.com>
2023-11-30 19:31:44 +0100

    fixed the previous issues

--- packages/contracts/src/PriceFeedImplement.sol
+++ packages/contracts/src/PriceFeedImplement.sol
@@ -3,96 +3,56 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
 
+import "./interfaces/IPriceObserver.sol";
 
-contract PriceFeedImplement {
+
+contract PriceFeedImplement is IPriceObserver {
 
     mapping(string => address) private dataFeedAddresses;
-
-    mapping(address => TokenInfo) private tokenInfos;
-
-    struct TokenInfo {
-        uint256 targetInitPrice;
-        uint256 targetKnockInPrice;
-        uint256 targetKnockOutPrice;
-        uint256 startTime;
-        uint256 period;
-        uint256 baseProfit;
-        address usdToken;
-    }
-    /**
-    * @notice Constructor that initializes the contract with Chainlink data feed addresses.
-    */
+    mapping(address => ProductInfo) private tokenInfos;
+    mapping(address => uint256) private currentInfo;
+    address[] private productAddresses; 
     constructor() {
-        // 使用新地址更新映射
-        dataFeedAddresses["BTC/USD"] = 0x007A22900a3B98143368Bd5906f8E17e9867581b;
-        dataFeedAddresses["DAI/USD"] = 0x0FCAa9c899EC5A91eBc3D5Dd869De833b06fB046;
-        dataFeedAddresses["ETH/USD"] = 0x0715A7794a1dc8e42615F059dD6e406A6594651A;
-        dataFeedAddresses["EUR/USD"] = 0x7d7356bF6Ee5CDeC22B216581E48eCC700D0497A;
-        dataFeedAddresses["LINK/MATIC"] = 0x12162c3E810393dEC01362aBf156D7ecf6159528;
-        dataFeedAddresses["LINK/USD"] = 0x1C2252aeeD50e0c9B64bDfF2735Ee3C932F5C408;
-        dataFeedAddresses["MATIC/USD"] = 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada;
-        dataFeedAddresses["SAND/USD"] = 0x9dd18534b8f456557d11B9DDB14dA89b2e52e308;
-        dataFeedAddresses["SOL/USD"] = 0xEB0fb293f368cE65595BeD03af3D3f27B7f0BD36;
-        dataFeedAddresses["USDC/USD"] = 0x572dDec9087154dC5dfBB1546Bb62713147e0Ab0;
-        dataFeedAddresses["USDT/USD"] = 0x92C09849638959196E976289418e5973CC96d645;
+
     }
 
-    /**
-    * @notice Retrieves the latest price for a given cryptocurrency pair.
-    * @param pair The cryptocurrency pair in string format (e.g., "BTC/USD").
-    * @return price The latest price of the given pair.
-    * @dev This function queries the Chainlink data feed associated with the given pair.
-    *      It reverts if the pair is not found in the mapping.
-    */
 
-    function getLatestPrice(string memory pair) public view returns (int) {
-        address dataFeedAddress = dataFeedAddresses[pair];
-        require(dataFeedAddress != address(0), "Data feed not found for this pair");
 
-        AggregatorV2V3Interface dataFeed = AggregatorV2V3Interface(dataFeedAddress);
-        (, int price, , , ) = dataFeed.latestRoundData();
-        return price;
+    function getLatestPrice() public {
+        for (uint i = 0; i < productAddresses.length; i++) {
+            address productAddr = productAddresses[i];
+            ProductInfo storage product = tokenInfos[productAddr];
+            AggregatorV2V3Interface dataFeed = AggregatorV2V3Interface(product.usdToken);
+            (, int256 price, , , ) = dataFeed.latestRoundData();
+            currentInfo[productAddr] = uint256(price);
+        }
     }
 
-    /**
-    * @dev Stores information for a specific token.
-    * @param targetToken The address of the target token.
-    * @param targetInitPrice Initial price of the target token.
-    * @param targetKnockInPrice Price at which the knock-in event happens.
-    * @param targetKnockOutPrice Price at which the knock-out event happens.
-    * @param startTime Start time of the contract.
-    * @param period Duration for which the contract is valid.
-    * @param baseProfit Base profit associated with the contract.
-    * @param usdToken Address of the USD token used for conversion.
-    */
-    function storeTokenInfo(
-        address targetToken,
-        uint256 targetInitPrice,
-        uint256 targetKnockInPrice,
-        uint256 targetKnockOutPrice,
-        uint256 startTime,
-        uint256 period,
-        uint256 baseProfit,
-        address usdToken
-    ) public {
-        tokenInfos[targetToken] = TokenInfo(
-            targetInitPrice,
-            targetKnockInPrice,
-            targetKnockOutPrice,
-            startTime,
-            period,
-            baseProfit,
-            usdToken
-        );
+
+    function getProductResult(address productAddr) 
+    external 
+    view 
+    returns(SnowbowResultStatus, uint256 period, uint256 endPrice) {
+        ProductInfo memory product = tokenInfos[productAddr];
+        uint256 currentPrice = currentInfo[productAddr];
+        SnowbowResultStatus status;
+
+        // 根据价格计算赌局状态
+        if (currentPrice >= product.targetKnockInPrice && currentPrice <= product.targetKnockOutPrice) {
+            status = SnowbowResultStatus.InAndOut;
+        } else if (currentPrice >= product.targetKnockOutPrice) {
+            status = SnowbowResultStatus.OnlyOut;
+        } else if (currentPrice <= product.targetKnockInPrice) {
+            status = SnowbowResultStatus.OnlyIn;
+        } else {
+            status = SnowbowResultStatus.NorInOrOut;
+        }
+        return (status, product.period, currentPrice); 
     }
 
-    /**
-    * @dev Retrieves stored information for a specific token.
-    * @param targetToken The address of the token to query information for.
-    * @return Token information including prices, time, and profit parameters.
-    */
-    function getTokenInfo(address targetToken) public view returns (TokenInfo memory) {
-        require(tokenInfos[targetToken].startTime != 0, "Token info not found");
-        return tokenInfos[targetToken];
+    function registerProduct(address productAddr, ProductInfo calldata productInfo )  external
+    {
+        tokenInfos[productAddr] = productInfo;
+        productAddresses.push(productAddr);
     }
 }
--- packages/contracts/src/interfaces/IPriceObserver.sol
+++ packages/contracts/src/interfaces/IPriceObserver.sol
@@ -10,6 +10,18 @@ interface IPriceObserver {
         OnlyOut,
         OnlyIn
     }
+    
+    struct ProductInfo {
+        uint256 targetInitPrice;
+        uint256 targetKnockInPrice;
+        uint256 targetKnockOutPrice;
+        uint256 startTime;
+        uint256 period;
+        uint256 baseProfit;
+        address usdToken;
+    }
+
+    
 
     // get the product status
     // return product status, valid period length, end price of target assets(if apply)
@@ -17,4 +29,13 @@ interface IPriceObserver {
         external
         view
         returns (SnowbowResultStatus, uint256 period, uint256 endPrice);
+
+
+    /**
+    * @dev Registers a new product with the given product information.
+    * @param productInfo The information about the product to be registered.
+    * @param productAddr The product address
+    */
+    function registerProduct(address productAddr,ProductInfo calldata productInfo) external;
+    
 }
