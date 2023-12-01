ee52b4aabee10a9f60c7d2437fd853ad9768962e
c <anyone.cui935@gmail.com>
2023-12-01 21:50:12 +0100

    adding test cases

--- packages/contracts/test/PriceFeedImplement.t.sol
+++ packages/contracts/test/PriceFeedImplement.t.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import "../src/PriceFeedImplement.sol";
 import "../src/interfaces/IPriceObserver.sol";
 
+
 contract PriceFeedImplementTest is Test {
     PriceFeedImplement priceFeed;
 
@@ -45,6 +46,21 @@ contract PriceFeedImplementTest is Test {
     }
 
     function testTimeBasedLogging() public {
+
+        IPriceObserver.ProductInfo memory productInfo = IPriceObserver.ProductInfo({
+            targetInitPrice: 100,
+            targetKnockInPrice: 150,
+            targetKnockOutPrice: 200,
+            startTime: block.timestamp,
+            period: 365 days,
+            baseProfit: 1000,
+            usdToken: address(0x007A22900a3B98143368Bd5906f8E17e9867581b)
+        });
+
+        address productAddr = address(0x123);
+
+        priceFeed.registerProduct(productAddr, productInfo);
+
         uint256 startTime = block.timestamp;
         uint256 interval = 5 minutes;
         uint256 endTime = startTime + 1 hours; // 测试持续时间
@@ -52,12 +68,25 @@ contract PriceFeedImplementTest is Test {
         while(block.timestamp < endTime) {
             // 模拟5分钟间隔
             vm.warp(block.timestamp + interval);
-            emit log("Logging after 5 minutes");
+            priceFeed.getLatestPrice();
 
             // 确保循环不会永远执行
             if (block.timestamp + interval > endTime) {
                 break;
             }
         }
+
+
+        (IPriceObserver.SnowbowResultStatus status, 
+        uint256 period, 
+        uint256 endPrice) = priceFeed.getProductResult(productAddr);
+        emit log("the value of period");
+        emit log_uint(period);
+        emit log("the value of endPrice");
+        emit log_uint(endPrice);
+        
+        assertTrue(uint(status) >= 0, "Status is not valid.");
+        assertEq(period, period, "Period does not match.");
+        assertTrue(endPrice >= 0, "End price is not valid.");
     }
 }
