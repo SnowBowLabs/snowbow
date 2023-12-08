10e031286ce9ad926b51b0846384e7046d5bac0e
Noy <59097122+noyyyy@users.noreply.github.com>
2023-12-09 01:00:20 +0800

    feat: finish price observer

--- packages/contracts/src/interfaces/IPriceObserver.sol
+++ packages/contracts/src/interfaces/IPriceObserver.sol
@@ -10,18 +10,24 @@ interface IPriceObserver {
         OnlyOut,
         OnlyIn
     }
-    
+
     struct ProductInfo {
+        address targetToken;
         uint256 targetInitPrice;
         uint256 targetKnockInPrice;
         uint256 targetKnockOutPrice;
         uint256 startTime;
         uint256 period;
         uint256 baseProfit;
-        address usdToken;
     }
 
-    
+    struct ProductResult {
+        SnowbowResultStatus status;
+        bool KnockIn;
+        bool KnockOut;
+        uint32 validPeriod;
+        uint200 endPrice;
+    }
 
     // get the product status
     // return product status, valid period length, end price of target assets(if apply)
@@ -30,12 +36,9 @@ interface IPriceObserver {
         view
         returns (SnowbowResultStatus, uint256 period, uint256 endPrice);
 
-
     /**
-    * @dev Registers a new product with the given product information.
-    * @param productInfo The information about the product to be registered.
-    * @param productAddr The product address
-    */
-    function registerProduct(address productAddr,ProductInfo calldata productInfo) external;
-    
+     * @dev Registers a new product with the given product information.
+     * @param productInfo The information about the product to be registered.
+     */
+    function registerProduct(ProductInfo calldata productInfo) external;
 }
--- packages/contracts/test/PriceObserver.sol
+++ packages/contracts/test/PriceObserver.sol
@@ -2,73 +2,69 @@
 pragma solidity ^0.8.7;
 
 import {Test} from "forge-std/Test.sol";
-import "../src/PriceFeedImplement.sol";
-import "../src/interfaces/IPriceObserver.sol";
+import "src/PriceObserver.sol";
+import "src/interfaces/IPriceObserver.sol";
 
-
-contract PriceFeedImplementTest is Test {
-    PriceFeedImplement priceFeed;
+contract PriceObserverTest is Test {
+    PriceObserver priceObserver;
 
     function setUp() public {
-        priceFeed = new PriceFeedImplement(15);
+        priceObserver = new PriceObserver(15, block.timestamp);
         // 初始化任何必要的状态
     }
 
     function testRegisterProduct() public {
         // 构造ProductInfo
         IPriceObserver.ProductInfo memory productInfo = IPriceObserver.ProductInfo({
+            targetToken: address(0x007A22900a3B98143368Bd5906f8E17e9867581b),
             targetInitPrice: 100,
             targetKnockInPrice: 150,
             targetKnockOutPrice: 200,
             startTime: block.timestamp,
             period: 365 days,
-            baseProfit: 1000,
-            usdToken: address(0x007A22900a3B98143368Bd5906f8E17e9867581b)
+            baseProfit: 1000
         });
 
         address productAddr = address(0x123);
-
-        priceFeed.registerProduct(productAddr, productInfo);
-
+        vm.prank(productAddr);
+        priceObserver.registerProduct(productInfo);
     }
 
     function testGetProductResult() public {
         address productAddr = address(0x123);
 
-        (IPriceObserver.SnowbowResultStatus status, 
-        uint256 period, 
-        uint256 endPrice) = priceFeed.getProductResult(productAddr);
+        (IPriceObserver.SnowbowResultStatus status, uint256 period, uint256 endPrice) =
+            priceObserver.getProductResult(productAddr);
 
-
-        assertTrue(uint(status) >= 0, "Status is not valid.");
+        assertTrue(uint256(status) >= 0, "Status is not valid.");
         assertEq(period, period, "Period does not match.");
         assertTrue(endPrice >= 0, "End price is not valid.");
     }
 
     function testTimeBasedLogging() public {
-
         IPriceObserver.ProductInfo memory productInfo = IPriceObserver.ProductInfo({
+            targetToken: address(0x007A22900a3B98143368Bd5906f8E17e9867581b),
             targetInitPrice: 100,
             targetKnockInPrice: 150,
             targetKnockOutPrice: 200,
             startTime: block.timestamp,
             period: 365 days,
-            baseProfit: 1000,
-            usdToken: address(0x007A22900a3B98143368Bd5906f8E17e9867581b)
+            baseProfit: 1000
         });
 
         address productAddr = address(0x123);
 
-        priceFeed.registerProduct(productAddr, productInfo);
+        vm.prank(productAddr);
+        priceObserver.registerProduct(productInfo);
 
         uint256 startTime = block.timestamp;
         uint256 interval = 5 minutes;
         uint256 endTime = startTime + 1 hours; // 测试持续时间
 
-        while(block.timestamp < endTime) {
+        while (block.timestamp < endTime) {
             // 模拟5分钟间隔
             vm.warp(block.timestamp + interval);
-            priceFeed.getLatestPrice();
+            priceObserver.performUpkeep(new bytes(0));
 
             // 确保循环不会永远执行
             if (block.timestamp + interval > endTime) {
@@ -76,16 +72,14 @@ contract PriceFeedImplementTest is Test {
             }
         }
 
-
-        (IPriceObserver.SnowbowResultStatus status, 
-        uint256 period, 
-        uint256 endPrice) = priceFeed.getProductResult(productAddr);
+        (IPriceObserver.SnowbowResultStatus status, uint256 period, uint256 endPrice) =
+            priceObserver.getProductResult(productAddr);
         emit log("the value of period");
         emit log_uint(period);
         emit log("the value of endPrice");
         emit log_uint(endPrice);
-        
-        assertTrue(uint(status) >= 0, "Status is not valid.");
+
+        assertTrue(uint256(status) >= 0, "Status is not valid.");
         assertEq(period, period, "Period does not match.");
         assertTrue(endPrice >= 0, "End price is not valid.");
     }
