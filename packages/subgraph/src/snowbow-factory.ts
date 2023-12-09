76483f653f649026ea3a8d4f8359b6949a01bc2a
Noy <59097122+noyyyy@users.noreply.github.com>
2023-12-10 04:45:53 +0800

    feat: finish graph basic logic and deploy

--- packages/subgraph/src/snowbow-factory.ts
+++ packages/subgraph/src/snowbow-factory.ts
@@ -1,6 +1,8 @@
 import { SnowbowProduct } from "../generated/templates";
 import { ProductCreate } from "../generated/SnowbowFactory/SnowbowFactory";
+import { fetchProduct } from "./snowbow-product";
 
 export function handleProductCreate(event: ProductCreate): void {
+  fetchProduct(event.params.param0);
   SnowbowProduct.create(event.params.param0);
 }
--- packages/subgraph/src/snowbow-product.ts
+++ packages/subgraph/src/snowbow-product.ts
@@ -1,21 +1,25 @@
-import { Address } from "@graphprotocol/graph-ts";
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 import {
-  OwnershipHandoverCanceled as OwnershipHandoverCanceledEvent,
-  OwnershipHandoverRequested as OwnershipHandoverRequestedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  ProductCreate as ProductCreateEvent,
-} from "../generated/SnowbowFactory/SnowbowFactory";
+  BuyShare,
+  SnowbowProduct,
+} from "../generated/templates/SnowbowProduct/SnowbowProduct";
 import { Product, User, UserProduct } from "../generated/schema";
 
-export function handleProductCreate(event: ProductCreateEvent): void {
-  // let entity = new ProductCreate(
-  //   event.transaction.hash.concatI32(event.logIndex.toI32())
-  // );
-  // entity.param0 = event.params.param0;
-  // entity.blockNumber = event.block.number;
-  // entity.blockTimestamp = event.block.timestamp;
-  // entity.transactionHash = event.transaction.hash;
-  // entity.save();
+const ETHER_DECIMAL = BigDecimal.fromString("1000000000000000000");
+const PRICE_DECIMAL_BIG_DECIMAL = BigDecimal.fromString("100000000");
+const YIELD_RATE_DECIMAL_BIG_DECIMAL = BigDecimal.fromString("10000");
+
+export function handleBuyShare(event: BuyShare): void {
+  const user = fetchUser(event.params.user);
+  const product = fetchProduct(event.address);
+
+  const up = fetchUserProduct(user, product);
+
+  up.holdingTargetAmount = up.holdingTargetAmount.plus(
+    new BigDecimal(event.params.targetAmount).div(ETHER_DECIMAL)
+  );
+
+  up.save();
 }
 
 export function fetchUser(userAddr: Address): User {
@@ -35,8 +39,44 @@ export function fetchProduct(prodAddr: Address): Product {
   if (product == null) {
     product = new Product(prodAddr);
     product.address = prodAddr;
+
+    const contract = SnowbowProduct.bind(prodAddr);
+
+    product.startTime = contract._startTime();
+    product.period = contract._period();
+    product.targetToken = contract._targetToken();
+    product.strikePrice = new BigDecimal(contract._targetInitPrice()).div(
+      PRICE_DECIMAL_BIG_DECIMAL
+    );
+    product.knockInPrice = new BigDecimal(contract._targetKnockInPrice()).div(
+      PRICE_DECIMAL_BIG_DECIMAL
+    );
+    product.knockOutPrice = new BigDecimal(contract._targetKnockOutPrice()).div(
+      PRICE_DECIMAL_BIG_DECIMAL
+    );
+
+    product.yieldRate = new BigDecimal(
+      BigInt.fromI32(contract._baseProfit())
+    ).div(YIELD_RATE_DECIMAL_BIG_DECIMAL);
   }
+
   product.save();
 
   return product;
+}
+
+export function fetchUserProduct(user: User, product: Product): UserProduct {
+  const id = `${user.address.toHexString()}-${product.address.toHexString()}`;
+  let up = UserProduct.load(id);
+
+  if (up == null) {
+    up = new UserProduct(id);
+    up.user = user.id;
+    up.product = product.id;
+    up.holdingTargetAmount = BigDecimal.zero();
+
+    up.save();
+  }
+
+  return up;
 }
