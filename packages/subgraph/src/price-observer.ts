fccc8ecc2dd46da2b741788ffbd27694615c3adc
Noy <59097122+noyyyy@users.noreply.github.com>
2023-12-10 21:04:38 +0800

    fix: subgraph product result

--- packages/subgraph/src/price-observer.ts
+++ packages/subgraph/src/price-observer.ts
@@ -7,15 +7,6 @@ import {
 } from "../generated/PriceObserver/PriceObserver";
 import { PRICE_DECIMAL_BIG_DECIMAL, fetchProduct } from "./snowbow-product";
 
-export enum SnowbowResultStatus {
-  Invalid,
-  NotEnd,
-  NorInOrOut,
-  InAndOut,
-  OnlyOut,
-  OnlyIn,
-}
-
 export function handleKnockIn(event: KnockIn): void {
   const product = fetchProduct(event.params.product);
   product.knockIn = true;
@@ -47,16 +38,24 @@ export function handleProductStatus(event: ProductStatusChange): void {
   switch (event.params.currentStatus) {
     case 0:
       product.result = "Invalid";
+      break;
     case 1:
       product.result = "NotEnd";
+      break;
     case 2:
       product.result = "NorInOrOut";
+      break;
     case 3:
       product.result = "InAndOut";
+      break;
     case 4:
       product.result = "OnlyOut";
+      break;
     case 5:
       product.result = "OnlyIn";
+      break;
+    default:
+      break;
   }
 
   product.save();
